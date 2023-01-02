0dbc33a145f6e134b9d4ac626198fcc5e0961a8c
Michael Dausmann <mdausmann@gmail.com>
2023-01-02 17:04:04 +1100

    Attempt a client side navigate post sign in

--- pages/login.vue
+++ pages/login.vue
@@ -9,6 +9,7 @@
     const { data, error } = await supabase.auth.signInWithOAuth({
       provider: 'google',
     })
+    navigateTo('dashboard'); // This doesn't work, it navigates prior to the login handshake completing and then the handshake lands on the index page and ignores the middleware.
   }
 </script>
 
