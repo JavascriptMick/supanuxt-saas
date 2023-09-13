72c7002698065a9d0e958285fdf588f0d177f216
Michael Dausmann <mdausmann@gmail.com>
2023-09-13 20:21:00 +1000

    cookie consent.  closes #13

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -2,5 +2,6 @@
   <div class="sticky z-50 bottom-0 p-4 bg-base-200">
     <span><NuxtLink to="/terms">Terms Of Service</NuxtLink></span>
     <span>&nbsp;|&nbsp;<NuxtLink to="/privacy">Privacy</NuxtLink></span>
+    <span>&nbsp;|&nbsp;<button type="button" data-cc="c-settings">Cookie settings</button></span>
   </div>
 </template>
