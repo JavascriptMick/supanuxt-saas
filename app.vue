a21cca64d535df93b68a2f3f9deac9fa8db32645
Michael Dausmann <mdausmann@gmail.com>
2024-02-17 12:15:06 +1100

    seo meta

--- app.vue
+++ app.vue
@@ -9,3 +9,19 @@
     <AppFooter />
   </div>
 </template>
+<script setup lang="ts">
+  useSeoMeta({
+    title: 'SupaNuxt SaaS',
+    ogTitle: 'SupaNuxt SaaS',
+    ogUrl: 'https://nuxt3-saas-boilerplate.netlify.app/',
+    ogType: 'website',
+    // fbAppId: '1111222233334444', // you will need this if you authenticate with fb
+    description:
+      'With SupaNuxt SaaS, you can easily get started building your next web application. Our pre-configured tech stack and industry leading features make it easy to get up and running in no time.',
+    ogDescription:
+      'With SupaNuxt SaaS, you can easily get started building your next web application. Our pre-configured tech stack and industry leading features make it easy to get up and running in no time.',
+    ogImage:
+      'https://nuxt3-saas-boilerplate.netlify.app/_nuxt/supanuxt_logo_200.896c0d73.png',
+    twitterCard: 'summary_large_image'
+  });
+</script>
